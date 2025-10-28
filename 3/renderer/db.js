dbc540549f99d51c5ac43ffed687fc03e503811c
BassaniMirko <167759821+BassaniMirko@users.noreply.github.com>
2025-10-28 14:44:45 +0100

    no password

--- 3/renderer/db.js
+++ 3/renderer/db.js
@@ -6,7 +6,7 @@
   let table = "students";
 
   const SUPABASE_URL = "https://ukaxvfohnynqjvgzxtkk.supabase.co";
-  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrYXh2Zm9obnlucWp2Z3p4dGtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MzU5NzgsImV4cCI6MjA3NjAxMTk3OH0.dZIYwmU-DYSgZFqmpEGXnwb8mm1pYGTU7As9ZrlFWL4";
+  const SUPABASE_ANON_KEY = ".....";
 
   const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
   let channel;
